beb306b77e7a9270a57f34d945b0b67e662a46f3
PumpkinDan <kfdsy0103@naver.com>
2024-08-17 14:02:44 +0900

    댓글관련 기능 추가

--- src/main/java/board/board/controller/ReplyController.java
+++ src/main/java/board/board/controller/ReplyController.java
@@ -7,10 +7,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -27,5 +24,32 @@ public class ReplyController {
         log.info("bno: " + bno);
 
         return new ResponseEntity<>(replyService.getList(bno), HttpStatus.OK); // 객체와 상태코드 전송
+    }
+
+    @PostMapping("")
+    public ResponseEntity<Long> register(@RequestBody ReplyDTO replyDTO) {
+        log.info("replyDTO: " + replyDTO);
+
+        Long rno = replyService.register(replyDTO);
+
+        return new ResponseEntity<>(rno, HttpStatus.OK);
+    }
+
+    @DeleteMapping("/{rno}")
+    public ResponseEntity<String> remove(@PathVariable("rno") Long rno) {
+        log.info("rno: " + rno);
+
+        replyService.remove(rno);
+
+        return new ResponseEntity<>("success", HttpStatus.OK);
+    }
+
+    @PutMapping("/{rno}")
+    public ResponseEntity<String> modify(@RequestBody ReplyDTO replyDTO) {
+        log.info("replyDTO: " + replyDTO);
+
+        replyService.modify(replyDTO);
+
+        return new ResponseEntity<>("success", HttpStatus.OK);
     }
 }
