dcfee2a40a33f3f99cb437a7e6497cb1b14343c2
PumpkinDan <kfdsy0103@naver.com>
2024-08-22 19:08:52 +0900

    이미지 파일 업로드/삭제 구현

---
+++ src/main/java/board/board/controller/UploadController.java
@@ -0,0 +1,148 @@
+package board.board.controller;
+
+import board.board.dto.UploadResultDTO;
+import lombok.extern.slf4j.Slf4j;
+import net.coobird.thumbnailator.Thumbnailator;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.util.FileCopyUtils;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+import java.net.URLDecoder;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+
+@RestController
+@Slf4j
+public class UploadController {
+
+    @Value("${board.board.upload.path}") // application.properties 의 변수
+    private String uploadPath;
+
+    @PostMapping("/uploadAjax")
+    public ResponseEntity<List<UploadResultDTO>> uploadFile(@RequestParam("uploadFiles") MultipartFile[] uploadFiles) {
+
+        List<UploadResultDTO> resultDTOList = new ArrayList<>();
+
+        for (MultipartFile uploadFile : uploadFiles) {
+
+            // 이미지 파일만 업로드 가능 (쉘 스크립트 공격 방지)
+            if(uploadFile.getContentType().startsWith("image") == false) {
+                log.warn("this file is not image type");
+                return new ResponseEntity<>(HttpStatus.FORBIDDEN);
+            }
+            // 실제 파일 이름, IE나 Edge는 전체 경로가 들어옴
+            String originalFilename = uploadFile.getOriginalFilename();
+            String fileName = originalFilename.substring(originalFilename.lastIndexOf("\\") + 1);
+            log.info("fileName: " + fileName);
+
+            // 날짜 폴더 생성 (폴더 저장 공간 분산)
+            String folderPath = makeFolder();
+
+            // UUID
+            String uuid = UUID.randomUUID().toString();
+
+            // uuid_파일이름
+            String saveName = uploadPath + File.separator + folderPath + File.separator + uuid + "_" + fileName;
+
+            Path savePath = Paths.get(saveName);
+
+            try {
+                uploadFile.transferTo(savePath);
+
+                // 썸네일 파일명은 _s로 시작
+                String thumbnailSaveName = uploadPath + File.separator + folderPath + File.separator + "s_" + uuid + "_" + fileName;
+                File thumbnailFile = new File(thumbnailSaveName);
+                // 썸네일 생성
+                Thumbnailator.createThumbnail(savePath.toFile(), thumbnailFile, 100, 100);
+
+                resultDTOList.add(new UploadResultDTO(fileName, uuid, folderPath));
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
+        return new ResponseEntity<>(resultDTOList, HttpStatus.OK);
+    }
+
+    @PostMapping("/removeFile")
+    public ResponseEntity<Boolean> removeFile(@RequestParam("fileName") String fileName) {
+
+        String srcFileName = null;
+
+        try {
+            srcFileName = URLDecoder.decode(fileName, "UTF-8");
+            
+            // 원본 파일
+            File file = new File(uploadPath + File.separator + srcFileName);
+            boolean result = file.delete();
+
+            // 썸네일 파일
+            File thumbnail = new File(file.getParent(), "s_" + file.getName());
+
+            result = thumbnail.delete();
+
+            return new ResponseEntity<>(result, HttpStatus.OK);
+
+        } catch (Exception e) {
+            e.printStackTrace();
+            return new ResponseEntity<>(false, HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    @GetMapping("/display")
+    public ResponseEntity<byte[]> getFile(@RequestParam("fileName") String fileName) {
+
+        ResponseEntity<byte[]> result = null;
+
+        try {
+            String srcFileName = URLDecoder.decode(fileName, "UTF-8");
+            log.info("fileName: " + fileName);
+
+            File file = new File(uploadPath + File.separator + srcFileName);
+            log.info("file: " + file);
+
+            HttpHeaders header = new HttpHeaders();
+            header.add("Content-type", Files.probeContentType(file.toPath()));
+
+            result = new ResponseEntity<>(FileCopyUtils.copyToByteArray(file), header, HttpStatus.OK);
+        } catch (Exception e) {
+            log.error(e.getMessage());
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+
+        return result;
+    }
+
+    private String makeFolder() {
+        String str = LocalDate.now().format(DateTimeFormatter.ofPattern("yyyy/MM/dd"));
+
+        String folderPath = str.replace("/", File.separator);
+
+        // 폴더 경로
+        File uploadPathFolder = new File(uploadPath, folderPath);
+
+        // 기존에 폴더가 존재하지 않으면 생성
+        if(uploadPathFolder.exists() == false) { 
+            uploadPathFolder.mkdirs();
+        }
+
+        return folderPath;
+    }
+}
